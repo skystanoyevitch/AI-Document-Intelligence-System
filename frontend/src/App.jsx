a98eb72c75f6d8b289ec801e7f175028136af8c7
Sky Stanoyevitch <skystanoyevitch@proton.me>
2025-09-16 13:58:49 -0700

    feat: implement main App component with ReceiptUploader

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReceiptUploader from "./components/ReceiptUploader";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ReceiptUploader />
+    </div>
+  );
+}
+
+export default App;
